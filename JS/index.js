b8d3dafea2ef351022402377d0013cd2864b33c6
Agustin Koltukian <agustin.koltukian@fing.edu.uy>
2021-08-31 15:40:01 -0300

    Primer commit

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+const elBoton = document.querySelector("button");
+const navMenu = document.querySelector(".nav_1");
+elBoton.addEventListener("click", () =>  {
+    navMenu.classList.toggle("nav_menu_visible");
+});
+feather.replace()
